63373547680d1901383af440afb0f4aa1b49d967
Ling Zhang <lingzhang@Lings-MBP.ht.home>
2022-11-28 22:33:12 -0500

    add passport strategy

--- server.js
+++ server.js
@@ -6,9 +6,31 @@ dotenv.config()
 const userService = require("./user-service.js")
 const jwt = require("jsonwebtoken")
 const passport = require("passport")
-
+const passportJWT = require("passport-jwt")
 const HTTP_PORT = process.env.PORT || 8080
 
+let ExtractJwt = passportJWT.ExtractJwt
+let JwtStrategy = passportJWT.Strategy
+
+let jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt")
+jwtOptions.secretOrKey = process.env.JWT_SECRET
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+	console.log("payload received", jwt_payload)
+
+	if (jwt_payload) {
+		next(null, {
+			_id: jwt_payload._id,
+			userName: jwt_payload.userName,
+		})
+	} else {
+		next(null, false)
+	}
+})
+passport.use(strategy)
+
+app.use(passport.initialize())
 app.use(express.json())
 app.use(cors())
 
